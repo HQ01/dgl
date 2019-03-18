007e71ba72c1d017b564d28642cc86c548ad9ca8
HQ01 <qh384@nyu.edu>
2019-03-18 16:56:27 +0800

    usecase updated

--- python/dgl/data/usecase_sync.py
+++ python/dgl/data/usecase_sync.py
@@ -2,5 +2,5 @@ import torch
 import dgl
 from dgl.data import sync_pool
 
-test_sync_pool = sync_pool.SyncPoolDataset(20, num_sub_graphs=10)
+test_sync_pool = sync_pool.SyncPoolDataset(20, num_sub_graphs=10, mode='val')
 candidate_graph, feature, label, n2sub_label = test_sync_pool[1]
